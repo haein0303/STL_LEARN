e0a703509dfb0ee135446b74c664e294fd4a72e9
NERU <2849593@naver.com>
2022-04-18 13:28:52 +0900

    220418

--- main.cpp
+++ main.cpp
@@ -1,59 +1,57 @@
 // ============================================================================
 //									STL
 // 
-//	2022.4.4. 월23              월23수34                   
+//	2022.4.18. 월23              월23수34                   
 // 
-// 컨테이너 - 다른객체를 저장하는 객체
-// sequence Container
-//   - array    유일하게 크기가 컴파일 타임에 결정
-//   - vector   dynamic array 
+//
+// 
 // ============================================================================
 
 
 #include <iostream>
-#include <deque>
+#include <list>
 #include <vector>
 #include <fstream>
 #include <algorithm>
+#include <string>
 #include "save.h"
 #include "STRING.h"
 
 using namespace std;
 
-extern bool 관찰;
-
-//[문제] 키보드에서 입력하는 모든 단어를 받아라
-//오름차순으로 정렬한다
-//결과를 출력한다.
-
-
+//[문제] "연습.txt"를
 
 int main() {
-	//save("main.cpp");
-	//[문제]
-	// 소스.cpp 파일의 단어를 deque<STRING>에 저장하라.
-	// 길이 오름차순으로 정렬하라
-	// 정렬된 결과를 출력하라
-	관찰 = true;
-	ifstream in{ "main.cpp" };
-	deque<string> d{};
-	string s;
-	while (in >> s) {
-		d.push_front(s.data());
-	}
+	save("main.cpp");
 
-	sort(d.begin(), d.end(), [](const string &a, const string &b){
-		return a.length() > b.length();
-	});
-
-	for (const auto &a : d) {
-		cout << a << endl;
-	}
+	ifstream in{ "연습.txt" };
 	
-	STRING a{ "asdasd" };
-	STRING b;
-	b = std::move(a);
+	list<string> cont;
+	string str;
+	
+	cout << "리스트의 크기 - " << sizeof(list<string>) << endl;
+
+	while (in >> str) {
+		cont.push_back(str);
+	}
 
 
+	auto p = find(cont.begin(), cont.end(), string{ "space" });
+	int step{};
+	auto beg = cont.begin();
+
+	
+	cout << "정렬전에는 " << distance(cont.begin(), p) + 1 << "번째 단어라네" << endl;
+	//문제
+	//cont를 ascending order로 정렬하라
+	//정렬된 결과를 앞에서부터 10개만 화면 출력하라
+
+	cont.sort([](const string& a, const string& b) {return a > b; });
+	
+	p = find(cont.begin(), cont.end(), string{ "space" });
+	cout << "정렬후에는 " << distance(cont.begin(), p) + 1 << "번째 단어라네" << endl;
+	
+	copy_if(cont.begin(), cont.end(), ostream_iterator<string>{cout,"\n"}, [](const string& a) {
+		return a.size() == 5; });
 
 }
