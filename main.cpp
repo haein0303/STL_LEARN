1fbdc25adcfdc5ea314c138b7afd76555154c5c9
NERU <2849593@naver.com>
2022-04-27 11:44:09 +0900

    220425

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 // ============================================================================
 //									STL
 // 
-//	2022.4.18. 월23              월23수34                   
+//	2022.4.27.        수34              월23수34               8주 2일     
 // 
-//
+//  
 // 
 // ============================================================================
 
@@ -21,37 +21,25 @@ using namespace std;
 
 //[문제] "연습.txt"를
 
+template <class iter>
+
+void show(iter) {
+	//[문제]iter의 category를 출력하라
+	//cout << typeid(iter).name() << endl;
+	cout << typeid(iterator_traits<iter>::iterator_category).name() << endl;
+	
+}
+
+
 int main() {
+	int* p = NULL;
+	//반복자 종류를 출력해본다 show() : 반복자 종류를 출력한다.
+	//템플릿 함수에는 뭐가 전달될지 모른다. - 알았으면 더 좋은 함수를 만들 수 있다.
+	show(p);
+	show(istream_iterator<char>{cin});
+	show(ostream_iterator<char>{cout});
+
+
+
 	save("main.cpp");
-
-	ifstream in{ "연습.txt" };
-	
-	list<string> cont;
-	string str;
-	
-	cout << "리스트의 크기 - " << sizeof(list<string>) << endl;
-
-	while (in >> str) {
-		cont.push_back(str);
-	}
-
-
-	auto p = find(cont.begin(), cont.end(), string{ "space" });
-	int step{};
-	auto beg = cont.begin();
-
-	
-	cout << "정렬전에는 " << distance(cont.begin(), p) + 1 << "번째 단어라네" << endl;
-	//문제
-	//cont를 ascending order로 정렬하라
-	//정렬된 결과를 앞에서부터 10개만 화면 출력하라
-
-	cont.sort([](const string& a, const string& b) {return a > b; });
-	
-	p = find(cont.begin(), cont.end(), string{ "space" });
-	cout << "정렬후에는 " << distance(cont.begin(), p) + 1 << "번째 단어라네" << endl;
-	
-	copy_if(cont.begin(), cont.end(), ostream_iterator<string>{cout,"\n"}, [](const string& a) {
-		return a.size() == 5; });
-
 }
