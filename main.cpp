a279752c12965da7160646be6ec33c3d1a2f4714
NERU <2849593@naver.com>
2022-05-16 12:20:27 +0900

    220516

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 // ============================================================================
 //									STL
 // 
-//	2022.5.11.        수34              월23수34               8주 2일     
+//	2022.5.16.        월23              월23수34               8주 2일     
 // 
 //  
 // 
@@ -13,6 +13,8 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <algorithm>
+#include <format>
 #include "save.h"
 #include "STRING.h"
 #include "my_Algorithm.h"
@@ -21,25 +23,27 @@ using namespace std;
 
 int main() {
 
-//[문제] "main.cpp"의 소문자 갯수를 세서 출력하라.
+//[문제] e-class에서 "frankenstein.txt"를 vector<string>에 읽어라
+	ifstream in{ "Frankenstein.txt" };
+	
+	vector<string> v{ istream_iterator<string>{in},{} };
 
-	//int alpha[26];
-	//
-	//char c = 'a';
-	//alpha['a' - 'a']++;
-
-	map<string, int> cim;
-
-	ifstream in{ "main.cpp" };
-	string tmp;
-	while (in>>tmp) {
-		cim[tmp]++;
+	//[문제] 많이 출현한 단어 순으로 출력
+	map<string, int> count;
+	for (string a : v) {
+		count[a]++;
 	}
 
-	for (const auto& a : cim) {
-		cout << "[" << a.first << "] = " << a.second << endl;
+
+	multimap<int, string,greater<int>> m;
+	for (auto [문자, 개수] : count) {
+		m.insert({ 개수,문자 });
 	}
 
+	for (auto [개수, 문자] : m) {
+		cout << "[" << format("{:^20}",문자) << "] = " << format("{:5}", 개수) << endl;
+	}
+	
 
 	save("main.cpp");
 }
