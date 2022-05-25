ef5576a12d4cbb793f8c4cc59e809040a5477b27
NERU <2849593@naver.com>
2022-05-25 13:19:52 +0900

    220525

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 // ============================================================================
 //									STL
 // 
-//	2022.5.18.        수34              월23수34               11주 2일     
-// 
-
+//	2022.5.25.        수34              월23수34               12주 2일     
+//
+//	알고리즘 - 어떤것들이 있나 살펴본다
 // 
 // 
 // ============================================================================
@@ -11,36 +11,30 @@
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <string>
-#include <thread>
-#include <format>
 #include "save.h"
 #include "STRING.h"
 #include "my_Algorithm.h"
 
 using namespace std;
 
-
+template<class iter>
+long long  dist(iter b, iter e) {
+	return e - b;
+}
 
 int main() {
 
 	save("main.cpp");
-	// erase-remove idiom
-	vector<int> v{ 1,2,3,4,5,6 };
 	
-	//v에서 홀수를 삭제하시오
-	// v.erase(remove_if(v.begin(), v.end(), [](int n) {return n % 2; }), v.end());
-	// remove_if 는 조건에 맞는 데이터를 지우는 연산이 아닌 뒤로 밀어줌
-	//			리턴값은 쓸모없어진 값의 시작 반복자
-	// erase를 통해서 지움
+	vector<int> v{ 1,2,3,4,5 };
 
-	//C++20에서는 전역 erase함수를 제공한다
-	erase_if(v, [](int n) {return n % 2; });
+	//[문제] 템플릿 함수 dist를 작성하라
 
-	for (int n : v) {
-		cout << n << endl;
-	}
+	cout << dist(v.begin(), v.end()) << endl;
+	cout << dist(v.end(), v.begin()) << endl;
+	cout << dist(1, 100) << endl;
+
+
 
 
 
