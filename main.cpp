187a1754c928c14a3c591d80e312152334d58998
NERU <2849593@naver.com>
2022-06-06 13:06:25 +0900

    20220606

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 // ============================================================================
 //									STL
 // 
-//	2022.5.25.        수34              월23수34               12주 2일     
+//	2022.6.6.        수34              월23수34               13주 2일     
 //
 //	알고리즘 - 어떤것들이 있나 살펴본다
 // 
@@ -10,40 +10,36 @@
 
 
 #include <iostream>
+#include <concepts>
 #include <string>
 #include <random>
+#include <format>
+#include <vector>
+#include <ranges>
+#include <algorithm>
 #include "save.h"
 #include "STRING.h"
 #include "my_Algorithm.h"
-
+ 
 using namespace std;
 
-default_random_engine dre;
-uniform_int_distribution<> uid(1,100);
-uniform_int_distribution<> uidName{ 'a','z' };
-struct Dog {
-	string name;
-	int n;
-	Dog() {
-		n = uid(dre);
-		for (int i = 0; i < 5; ++i) {
-			name += uidName(dre);
-		}
-	}
-};
+
 int main() {
 
 	save("main.cpp");
-	
-	vector<Dog> v(100);
 
-	ranges::sort(v, {}, &Dog::n);
-
-	for (auto a : v) {
-		cout << a.name << " - " << a.n << endl;
+	int a[]{ 1,2,3,4,5,6,7,8,9,10 };
+	//[문제] 1부터 시작하는 자연수에서 range a에서 홀수만 골라 제곱하여 출력하라
+	auto 홀수 = [](int n) {return n % 2; };
+	auto 제곱 = [](int n) {return n * n; };
+	for (int n : views::iota(1)
+				| views::filter(홀수) 
+				| views::transform(제곱)
+				| views::take(10000)
+				| views::reverse
+		)
+	{
+		cout << format("{:10}", n) << " ";
 	}
-
-
-
 
 }
