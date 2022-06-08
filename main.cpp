78c8178c8089f5a590e726a2f52e8ea9e19e69bc
NERU <2849593@naver.com>
2022-06-08 13:19:49 +0900

    220608

--- main.cpp
+++ main.cpp
@@ -12,34 +12,43 @@
 #include <iostream>
 #include <concepts>
 #include <string>
-#include <random>
 #include <format>
 #include <vector>
 #include <ranges>
 #include <algorithm>
+#include <fstream>
+#include <set>
 #include "save.h"
 #include "STRING.h"
 #include "my_Algorithm.h"
  
 using namespace std;
 
+struct WP : pair<string,string> { //wordPair
+	WP(string s) : pair<string, string>{ s,s } {
+		sort(first.begin(), first.end());
+	};
+
+
+};
+
+
 
 int main() {
 
 	save("main.cpp");
 
-	int a[]{ 1,2,3,4,5,6,7,8,9,10 };
-	//[문제] 1부터 시작하는 자연수에서 range a에서 홀수만 골라 제곱하여 출력하라
-	auto 홀수 = [](int n) {return n % 2; };
-	auto 제곱 = [](int n) {return n * n; };
-	for (int n : views::iota(1)
-				| views::filter(홀수) 
-				| views::transform(제곱)
-				| views::take(10000)
-				| views::reverse
-		)
+	ifstream in{ "단어들.txt" };
+	vector<WP> v(istream_iterator<string>{ in }, {}); //단어 수 369882
+	
+	ranges::sort(v, {}, &WP::first);
+	for (WP words : v 
+		| views::drop(10'000)
+		| views::take(50)
+		) 
 	{
-		cout << format("{:10}", n) << " ";
+		cout << words.first << " -- " << words.second << endl;
 	}
+
 
 }
